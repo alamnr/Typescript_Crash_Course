a55c16abef8670b7dace2f19901ac4976a84499e
nalam <alamnr@gmail.com>
2018-02-06 17:41:26 +0600

    Initial commit

---
+++ types.ts
@@ -0,0 +1,35 @@
+//console.log('Hello World')
+
+let myString:string;
+let myNum:number;
+let myBool:boolean
+let myvar:any
+
+/*
+let strArray: string[]
+let numArray:number[]
+let boolArr:boolean[]
+*/
+
+let strArray:Array<string>
+let numArray:Array<number>
+let boolArr:Array<boolean>
+
+let strNumTuple:[string , number]
+
+myString = 'Hello World'.slice(0,5);
+myNum = 22+5;
+myBool = false
+//myString = 1
+
+myvar ='adasdasd'
+
+strArray =  ['Hello','world']
+numArray = [1,2,3]
+boolArr = [true, false]
+
+strNumTuple = ['Hello',4]
+
+let myVoid:void=undefined
+
+console.log(myVoid);
